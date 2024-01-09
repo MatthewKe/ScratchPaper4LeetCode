aa913ba4db0b0d668a3c8cc2a938e8d5d20a38ee
CureVoid <106899856+CureVoid@users.noreply.github.com>
2024-01-09 16:37:48 +0800

    修一点点输出区

--- backend/backend/src/main/java/sourcecode/Main.java
+++ backend/backend/src/main/java/sourcecode/Main.java
@@ -1,32 +1,7 @@
 package sourcecode;
 import com.example.backend.service.TreeNode;
-public class Main {
-    public static void main(String[] args) {
-        Solution solution = new Solution();
-        solution.constructMaximumBinaryTree(new int[]{3,2,1,6,0,5});
-        System.out.println("hello world");
-    }
-}
-
-
 class Solution {
-    public TreeNode constructMaximumBinaryTree(int[] nums) {
-        return construct(nums, 0, nums.length - 1);
-    }
-
-    public TreeNode construct(int[] nums, int left, int right) {
-        if (left > right) {
-            return null;
-        }
-        int best = left;
-        for (int i = left + 1; i <= right; ++i) {
-            if (nums[i] > nums[best]) {
-                best = i;
-            }
-        }
-        TreeNode node = new TreeNode(nums[best]);
-        node.setLeft(construct(nums, left, best - 1));
-        node.setRight(construct(nums, best + 1, right));
-        return node;
+    public int[] twoSum(int[] nums, int target) {
+        return new int[]{};
     }
 }
